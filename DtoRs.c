6c16e68cc79a8213efcbc30c5c17bca68a3807df
HarshBhanderi07 <harshbhanderijkn@gmail.com>
2025-08-31 10:21:52 +0530

    Add files via upload

---
+++ DtoRs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float $, ₹;
+    printf("Enter amount of Dollars:");
+    scanf("%f",&$);
+
+    ₹ = 45 * $ ;
+    printf("Amount in Rupees: %f",₹);
+    return 0;
+}
