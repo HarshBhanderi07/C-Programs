f2c3d4ab897eb270caa2c57343b0554bd8b14770
HarshBhanderi07 <harshbhanderijkn@gmail.com>
2025-08-31 09:33:56 +0530

    Add files via upload

---
+++ Dividenum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+
+{
+    int  x,y,Divide;
+    printf("Enter first number: ");
+    scanf("%d",&x);
+    printf("Enter second number: ");
+    scanf("%d",&y);
+    Divide=x/y;
+    printf("Divide=%d\n",Divide);
+    return 0;
+}
