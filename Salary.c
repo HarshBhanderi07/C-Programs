1d912be4ae202af443648d4af7efdad1e6ae215c
HarshBhanderi07 <harshbhanderijkn@gmail.com>
2025-08-31 11:20:13 +0530

    Add files via upload

---
+++ Salary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float gross_salary, allowance, deduction, net_salary;
+
+    printf("Enter gross salary: ");
+    scanf("%f", &gross_salary);
+
+    allowance = gross_salary * 0.10;
+    deduction = gross_salary * 0.03;
+
+    net_salary = gross_salary + allowance - deduction;
+
+    printf("Net Salary = %.2f\n", net_salary);
+
+    return 0;
+}
