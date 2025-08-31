c6d331eec54c2f0c44f16a449e588988af6aae98
HarshBhanderi07 <harshbhanderijkn@gmail.com>
2025-08-31 08:38:26 +0530

    Add files via upload

---
+++ Addtwonumbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+    int x, y, sum;
+
+    
+    printf("Enter first number: ");
+    scanf("%d", &x);
+
+    printf("Enter second number: ");
+    scanf("%d", &y);
+
+    
+    sum = x + y ;
+
+    
+    printf("Sum = %d\n", sum);
+
+
+    return  0;
+}
